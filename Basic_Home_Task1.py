f8bd55caf1eb24eb7d508dfccbd4eb893fdea06c
ShubhraAg <105977921+ShubhraAg@users.noreply.github.com>
2023-07-21 18:20:44 +0530

    Update Basic_Home_Task1.py

--- Basic_Home_Task1.py
+++ Basic_Home_Task1.py
@@ -1,3 +1,15 @@
+# 1. Predict the output
 S1 = "Hello"
 S2 = "This is Python"
 print(len(S1), len(S2))
+
+# 2. WAP to input a string and print its length.
+str = input("Provide any string")
+a: int = len(str)
+print('the length of string is', a)
+
+# 3. WAP to input 2 numbers and print their sum and difference.
+a = input("Enter any integer")
+b = input("Enter second integer")
+print('Sum of numbers is ', a+b)
+print('Difference of numbers is ', a-b)
